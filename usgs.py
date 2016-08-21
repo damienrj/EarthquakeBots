d645bc03fc61aeaf9d5ed2fb6a6a8b2fc11c5174
damienrj <damienrj@gmail.com>
2016-08-21 13:22:19 -0700

    now prints to log

--- usgs.py
+++ usgs.py
@@ -21,51 +21,55 @@ def readfeed(bots, feed_url = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/
             #Check if tweet is in datebase
             connection = sqlite.connect(dbfile)
             cur = connection.cursor()
-            
+
             #Check if the quake was tweeted
             tweet_test = cur.execute('SELECT tweet FROM QUAKES WHERE code == ' + dict_input['code']).fetchall()
             quake_time = time.asctime(time.localtime(dict_input['time']/1000))[:-5]
-            message = str(dict_input['mag']) + ' earthquake ' + dict_input['place'] + ' at ' + quake_time + ' PT. ' + dict_input['url'] 
-            
+            message = str(dict_input['mag']) + ' earthquake ' + dict_input['place'] + ' at ' + quake_time + ' PT. ' + dict_input['url']
+
             dict_input['tweet'] = 1
             dict_input['tweet_time'] = time.asctime(time.gmtime())
-            dict_input['tweet_text'] = message 
-            
+            dict_input['tweet_text'] = message
+
             #Checks if quake was missing, or present but not tweeted
-            
-            if 'Washington' in dict_input['place'] and 'Washington' in bots:                
-                
+
+            if 'Washington' in dict_input['place'] and 'Washington' in bots:
+
                 correct_bot=bots['Washington']
             elif 'California' in dict_input['place'] and 'California' in bots:
                 correct_bot=bots['California']
             else:
-                #If there is no correct bot, close the connection and continue                
-                connection.commit()    
+                #If there is no correct bot, close the connection and continue
+                connection.commit()
                 connection.close()
                 continue
             #Insert values into database
-            
-            if  len(tweet_test)  == 0:            
+
+            if  len(tweet_test)  == 0:
                 insertdb(dict_input)
                 correct_bot.tweet(message, dict_input['latitude'], dict_input['longitude'])
+                print(message)
+                print('working')
             elif (len(tweet_test) > 0 and tweet_test[0][0]==0):
                 #This section is in case a earthquake was seen but missed and
                 #not tweeted about
-            
+
                 #Update tweet values
-                cur.execute("UPDATE QUAKES SET tweet = 1, tweet_time = '" 
+                cur.execute("UPDATE QUAKES SET tweet = 1, tweet_time = '"
                     + dict_input['tweet_time'] +
-                    "', tweet_text = '" + dict_input['tweet_text'] + 
+                    "', tweet_text = '" + dict_input['tweet_text'] +
                     "' WHERE code == " + dict_input['code'])
-                correct_bot.tweet(message, dict_input['latitude'], dict_input['longitude'])    
-            connection.commit()    
+                correct_bot.tweet(message, dict_input['latitude'], dict_input['longitude'])
+                print(message)
+                print('elif working')
+            connection.commit()
             connection.close()
-        
+
 #tweet_time = ' + time.asctime(time.gmtime())
 def insertdb(dict_input,dbfile='quakeBotDB.sqlite'):
     # insert info in the database. Takes in a dictionary of keys and
     # values and inserts those into the database
-    
+
     connection = sqlite.connect(dbfile)
     cur = connection.cursor()
 
@@ -91,7 +95,7 @@ def valid_location(dict_input):
         return True
     else:
         return False
-        
+
 def in_socal(gps):
     #Define the line between Ventura and Las Vegas to be Southen California
     #34.2750 N, 119.2278 W Ventura
@@ -102,9 +106,9 @@ def in_socal(gps):
         return(True)
     else:
         return(False)
-        
 
 
-    
+
+
 if __name__ == '__main__':
     print('Testing area')
